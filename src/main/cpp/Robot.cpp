163f37e27a9b56b1d8cb2ccbdfc8dfa479b660a9
dylantrwatson <dylantrwatson@gmail.com>
2019-04-23 19:46:56 -0500

    started cleaning. got to line 111 of Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -45,61 +45,42 @@ void Robot::RobotInit()
 	Log::restartfile();
 	Log::General("Program Version: " + to_string(VERSION) + " Revision: " + REVISION, true);
 	Config *config = new Config(m_activeCollection, m_drive); //!< Pointer to the configuration file of the robot
-	//Must have this for smartdashboard to work properly
-	SmartDashboard::init();
-	m_inst = nt::NetworkTableInstance::GetDefault();		  //!Network tables
+	SmartDashboard::init(); //!< Must have this for smartdashboard to work properly
+	m_inst = nt::NetworkTableInstance::GetDefault(); //!< Network tables
 	m_dashboardTable = m_inst.GetTable("DASHBOARD_TABLE");
 	m_dashboardTable->PutStringArray("AUTON_OPTIONS", m_autonOptions);
 	m_dashboardTable->PutStringArray("POSITION_OPTIONS", m_positionOptions);
-
-	//TODO: put this in some sort of config
-	/*m_visionTable->PutNumber("LS",0);
-	m_visionTable->PutNumber("US",3);
-	m_visionTable->PutNumber("LH",87);
-	m_visionTable->PutNumber("UH",126);
-	m_visionTable->PutNumber("LV",255);
-	m_visionTable->PutNumber("UV",255);
-	m_visionTable->PutNumber("MinA",1112);
-	m_visionTable->PutNumber("MaxA",82763);
-	m_visionTable->PutNumber("MaxO",62);
-	//know how computer graphics cordinate system works when editing these
-	m_visionTable->PutNumber("UPPER_BOUND",0); //top Y bound
-	m_visionTable->PutNumber("LOWER_BOUND",1000); //bottom Y bound
-	m_visionTable->PutNumber("LEFT_BOUND",0); //left X bound
-	m_visionTable->PutNumber("RIGHT_BOUND",1000); //right X bound*/
 }
 
 /*
  * Method that runs at the beginning of the autonomous mode
  * Uses the SmartDashboard to select the proper Autonomous mode to run
-  TODO: Integrate with our dashboad; Integrate with TeleOp for sandstorm; make everything that should be abstract is in fact abstract; Integrate vision overlay and drive cam to dashboard; Write teleop goals;
  */
-
 void Robot::Autonomous()
 {
 
 	m_masterGoal = new MultitaskGoal(m_activeCollection, false);
 
-	cout << "Autonomous Started." << endl;
+	Log::General("Autonomous Started");
 #ifndef _Win32
 	string autoSelected = m_dashboardTable->GetString("AUTON_SELECTION", m_driveStraight);
-	string positionSelected = m_dashboardTable->GetString("POSITION_SELECTION", "NONE"); //if it is none, then just drive straight
-	cout << autoSelected << endl;
-	if (!SelectAuton(m_activeCollection, m_masterGoal, autoSelected, positionSelected)) //!SELECTION
+	string positionSelected = m_dashboardTable->GetString("POSITION_SELECTION", "NONE"); // Default auto is drive striaght 
+	if (!SelectAuton(m_activeCollection, m_masterGoal, autoSelected, positionSelected)) // Selection
 	{
 		m_dashboardTable->PutString("AUTON_FOUND", "UNDEFINED AUTON OR POSITION SELECTED");
 	}
 #endif
 	m_masterGoal->AddGoal(new Goal_TimeOut(m_activeCollection, 15.0));
-	m_masterGoal->AddGoal(new Goal_ControllerOverride(m_activeCollection));
-	//m_masterGoal->Activate();
+	m_masterGoal->AddGoal(new Goal_ControllerOverride(m_activeCollection)); //!< This is for FRC 2019 SANDSTORM! Be aware that if Sandstorm is removed, this NEEDS to be removed.
+	//TODO: Make the auto configurable (turn on/turn off) OR add a no auton feature to the dashboard
+	m_masterGoal->Activate();
 	m_activeCollection->SetActiveGoal(m_masterGoal);
 	double dTime = 0.010;
 	double current_time = 0.0;
 	while (m_masterGoal->GetStatus() == Goal::eActive && _IsAutononomous() && !IsDisabled())
 	{
-		m_drive->Update(dTime);
-		m_masterGoal->Process(dTime);
+		m_drive->Update(dTime); //!< Check for controller input for controller override -> will be removed if sandstorm is removed
+		m_masterGoal->Process(dTime); //!< Process the autonomous goal
 		current_time += dTime;
 #ifdef _Win32
 		SmartDashboard::PutNumber("DELTA TIME", current_time);
@@ -107,25 +88,26 @@ void Robot::Autonomous()
 #endif		
 		Wait(dTime);
 	}
-	while(_IsAutononomous() && !IsDisabled())
+	/* THIS LOOP WILL HAVE TO BE REMOVED IF SANDSTORM IS REMOVED */
+	while(_IsAutononomous() && !IsDisabled()) //!< Loop to run manual control once the auto function is done in sandstorm
 	{
 #ifdef _Win32
 		SmartDashboard::PutString("AUTONOMOUS", "OVERRIDEN!");
 #endif
-		m_drive->Update(dTime);
+		m_drive->Update(dTime); //!< Run robot teleOp
 		Wait(dTime);
 	}
-	m_masterGoal->~MultitaskGoal();
-	Log::General("goal loop complete");
+	m_masterGoal->~MultitaskGoal(); //!< Destroy the masterGoal in order to prevent multiple ControllerOverrides running at once
 }
 
 /*
  * Called when teleop starts
  */
 #if 1 //test method doesnt work in rio for some reason...
+//TODO: Potentially make a "test" tag in the config that can toggle this?
 void Robot::OperatorControl()
 {
-	m_activeCollection->GetActiveGoal()->~MultitaskGoal();
+	m_activeCollection->GetActiveGoal()->~MultitaskGoal(); //!< Destroy any pre-existing masterGoal that was not properly disposed of
 	m_teleOpMasterGoal = new MultitaskGoal(m_activeCollection, false);
 	//m_teleOpMasterGoal->AddGoal(new Goal_TimeOut(m_activeCollection, 15));
 	//m_teleOpMasterGoal->AddGoal(new Goal_ControllerOverride(m_activeCollection));
