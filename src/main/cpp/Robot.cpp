cc9821c28832b00713abec1f359ac8f5465d7119
Dylan Watson <watson.dylan@centaurisoftware.co>
2020-01-09 13:36:38 -0600

    testing the build server with working code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -210,3 +210,5 @@ void Robot::Disabled() { /*Do nothing for now*/ }
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }  //!< This identifies Robot as the main Robot starting class
 #endif
+
+//TESING THE BUILD SERVER
