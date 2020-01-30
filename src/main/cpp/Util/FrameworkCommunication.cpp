b666f1a780866a57c266b5270ba33a3f9eb08e6c
Dylan Watson <watson.dylan@centaurisoftware.co>
2020-01-30 11:12:33 -0600

    Initial completion of FrameworkCommunication without RobotStatus event. Untested.

--- src/main/cpp/Util/FrameworkCommunication.cpp
+++ src/main/cpp/Util/FrameworkCommunication.cpp
@@ -12,6 +12,12 @@ Email: cooper.ryan@centaurisoftware.co, dylantrwatson@gmail.com
 Special thanks to pazdera on GitHub for the inspiration on Singleton mechanics
 \********************************************************************/
 
+#include <networktables/NetworkTableEntry.h>
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/NetworkTable.h>
+#include "Constants.h"
+#include "LoopChecks.h"
+
 using namespace std;
 
 //TODO: Check for memory leaks
@@ -25,13 +31,55 @@ namespace Util{
                 return instance;
             }
 
-        private:
-            FrameworkCommunication(){}
+            void SendData(string key, double value, bool announcePeriod = true){
+                if(_IsAutononomous() && announcePeriod)
+                    dashboardComm->PutNumber("AUTON_"+key,value);
+                else if(_IsTeleoporated() && announcePeriod)
+                    dashboardComm->PutNumber("TELEOP_"+key,value);
+                else
+                    dashboardComm->PutNumber(key,value);
+            }
 
+            void SendData(string k, int v, bool aP = true){
+                SendData(k,(double)v,aP);
+            }
+
+            void SendData(string k, string v, bool aP = true){
+                SendData(k,nt::Value::MakeString(v),aP);
+            }
+
+            void SendData(string k, bool v, bool aP = true){
+                SendData(k,nt::Value::MakeBoolean(v),aP);
+            }
+
+            void SendHealthData(string key, string value){
+                dashboardComm->PutValue("H_"+key,nt::Value::MakeString(value));
+            }
+
+            void SendHealthData(string key, double value){
+                SendHealthData(key, to_string(value));
+            }
+
+        private:
+            shared_ptr<nt::NetworkTable> dashboardComm;
+            FrameworkCommunication();
+            void SendData(string key, shared_ptr<nt::Value> value, bool announcePeriod = true){
+                if(_IsAutononomous() && announcePeriod)
+                    dashboardComm->PutValue("AUTON_"+key,value);
+                else if(_IsTeleoporated() && announcePeriod)
+                    dashboardComm->PutValue("TELEOP_"+key,value);
+                else
+                    dashboardComm->PutValue(key,value);
+            }
         public:
             FrameworkCommunication(FrameworkCommunication const&) = delete;
             void operator=(FrameworkCommunication const&) = delete;
 
     };
+
+    FrameworkCommunication::FrameworkCommunication(){
+        auto inst = nt::NetworkTableInstance::GetDefault();
+        dashboardComm = inst.GetTable(DASHBOARD_NETWORK_TABLE);
+    }
 
 }
