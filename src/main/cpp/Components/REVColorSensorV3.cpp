22f85088742640c9e393a86ea97afb68af4192c2
BBotz Computer <resources@broncbotz.org>
2020-02-16 00:20:58 -0600

    push

--- src/main/cpp/Components/REVColorSensorV3.cpp
+++ src/main/cpp/Components/REVColorSensorV3.cpp
@@ -17,6 +17,8 @@ Email: Shruti.venkat05@gmail.com
 \********************************************************************/
 
 #include "REVColorSensorV3.h"
+#include "rev/ColorMatch.h"
+
 
 using namespace rev;
 using namespace std;
@@ -27,6 +29,40 @@ REVColorSensorV3::REVColorSensorV3(string _name)
 	: InputComponent(_name){
 	Color = new ColorSensorV3(frc::I2C::Port::kOnboard);
 	Name = _name;
+}
+
+
+rev::ColorMatch m_colorMatcher;
+
+  static constexpr frc::Color kBlueTarget = frc::Color(0.143, 0.427, 0.429);
+  static constexpr frc::Color kGreenTarget = frc::Color(0.197, 0.561, 0.240);
+  static constexpr frc::Color kRedTarget = frc::Color(0.561, 0.232, 0.114);
+  static constexpr frc::Color kYellowTarget = frc::Color(0.361, 0.524, 0.113);
+
+ 
+  string GetColorMatch() {
+    m_colorMatcher.AddColorMatch(kBlueTarget);
+    m_colorMatcher.AddColorMatch(kGreenTarget);
+    m_colorMatcher.AddColorMatch(kRedTarget);
+    m_colorMatcher.AddColorMatch(kYellowTarget);
+  }
+void REVColorSensorV3:: Get(){
+
+  if (matchedColor == kBlueTarget) {
+      colorString = "Blue"; 
+    }
+     else if (matchedColor == kRedTarget) {
+      colorString = "Red";
+    } 
+    else if (matchedColor == kGreenTarget) {
+      colorString = "Green";
+    } else if (matchedColor == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
+
+    return();
 }
 
 frc::Color REVColorSensorV3::GetColor(){
